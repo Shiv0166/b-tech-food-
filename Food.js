90ca506f332ee62a368c6d31cea92b8a13bbdba1
Shiv0166 <shivamrajpoot0616@gmail.com>
2025-04-28 21:24:52 +0530

    new

--- Food.js
+++ Food.js
@@ -3,4 +3,18 @@ let logBtn = document.getElementById("logBtn");
  logBtn.addEventListener("click", function(){
     document.querySelector(".loginPage").style.display="block";
  })
+ addEventListener("click", function(){
+
+    let email = document.getElementById("name");
+    let pass = document.getElementById("pass");
+
+    if(email.value == "" || pass.value == ""){
+        alert("Please Enter Email Password")
+    }else{
+        alert("You Loged In");
+        document.querySelector(".loginPage").style.display="none";
+
+    }
+
+ })
  
