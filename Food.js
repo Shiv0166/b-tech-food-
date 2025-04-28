8e84853f05f6a8a3692d94d57cc248db766f9203
Shiv0166 <shivamrajpoot0616@gmail.com>
2025-04-28 21:31:26 +0530

    new

--- Food.js
+++ Food.js
@@ -54,3 +54,15 @@ let logBtn = document.getElementById("logBtn");
         
             document.getElementById('total-price').textContent = totalPrice.toFixed(2);
         }
+        const toggleFormButton = document.getElementById('toggleFormButton');
+        const feedbackForm = document.getElementById('form-container');
+
+        toggleFormButton.addEventListener('click', () => {
+            if (feedbackForm.style.display === 'none'  || feedbackForm.style.display === '') {
+                feedbackForm.style.display = 'block';
+                toggleFormButton.innerText = 'Hide Feedback Form';
+            } else {
+                feedbackForm.style.display = 'none';
+                toggleFormButton.innerText = 'Show Feedback Form';
+            }
+        });
